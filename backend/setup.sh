06c20b4c7e423992e6f25942472c51a29f172699
Vipul <vipullahepatil@gmail.com>
2025-11-12 01:08:19 +0530

    changes

--- backend/setup.sh
+++ backend/setup.sh
@@ -14,7 +14,8 @@ source venv/bin/activate
 
 # Install dependencies
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements.txt && python manage.py collectstatic --noinput
+
 
 # Apply migrations
 python3 manage.py migrate
